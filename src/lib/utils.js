99d3982c70cb678c71278e7029a01263cd3352b8
SmilePineapple <jakedalerourke@gmail.com>
2024-11-12 15:41:03 +0000

    Fix utils.js to plain text format

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,15 +1,7 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2820
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-\f0\fs24 \cf0 import \{ clsx \} from "clsx";\
-import \{ twMerge \} from "tailwind-merge";\
-\
-// The `cn` function combines and deduplicates Tailwind CSS classes.\
-export function cn(...inputs) \{\
-  return twMerge(clsx(inputs));\
-\}\
+// This function combines and deduplicates Tailwind CSS classes.
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
 }
