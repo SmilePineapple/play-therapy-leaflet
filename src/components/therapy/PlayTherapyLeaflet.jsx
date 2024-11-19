a0c06bd70f426366a91f3f62bd32c8cf0eb9f569
SmilePineapple <jakedalerourke@gmail.com>
2024-11-19 10:30:16 +0000

    Fix syntax error in print-specific styles

--- src/components/therapy/PlayTherapyLeaflet.jsx
+++ src/components/therapy/PlayTherapyLeaflet.jsx
@@ -137,7 +137,7 @@ const PlayTherapyLeaflet = () => {
 
   useEffect(() => {
   setNotes(
-    During our play session together, I will be hoping to learn all I can about you. All you need to do is play. I want to help and support the important adults in your life. I really look forward to meeting you ${childName}. See you on ${sessionDay}.
+    `During our play session together, I will be hoping to learn all I can about you. All you need to do is play. I want to help and support the important adults in your life. I really look forward to meeting you ${childName}. See you on ${sessionDay}.`
   );
 }, [childName, sessionDay]);
 
